45de7cdd677270c29d9c855b7512f91b2b2a27e8
William Mckee <99625972+williammckee-techtorium@users.noreply.github.com>
2022-03-28 19:09:28 +1300

    start app

---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-4xl text-center my-8">About</h1>
+<p>This is the about page.</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+
+<script>
+  import {pokemon, fetchPokemon} from "../stores/pokestore";
+  import {onMount} from "svelte";
+  console.log($pokemon)
+  onMount(() => {
+    fetchPokemon();
+  });
+
+</script>
+<svelte:head>
+<title>First Life</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8">First Life</h1>
+
+{#if $pokemon != null}
+{#each $pokemon as pokeman}
+
+<p>{pokeman.name}</p>
+{/each}
+{/if}
